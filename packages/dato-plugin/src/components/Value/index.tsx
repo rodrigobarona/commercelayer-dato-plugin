7488fd550bd19cdf1293c72d8c2f20a65e5c67c1
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2024-07-30 12:27:37 +0100

    Revert "remove react"
    This reverts commit 48e2b0c90f62f3d9afd9aad0dcdd8c92040efa79.

--- packages/dato-plugin/src/components/Value/index.tsx
+++ packages/dato-plugin/src/components/Value/index.tsx
@@ -27,6 +27,8 @@ const renderMetadata = (metadata: { [key: string]: string }) => {
   ));
 };
 
+import React from "react";
+
 const renderPrices = (prices: any[] | undefined) => {
   if (!prices || prices.length === 0) return null;
 
